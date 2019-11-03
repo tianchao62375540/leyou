b540c1cf39cfd2fac59a60fe66ff48138789e9b2
tc_tc_tc <455433520@qq.com>
2019-11-03 23:22:04 +0800

    商品规格组和规格

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -19,6 +19,7 @@ public enum ExceptionEnum {
     BRAND_NOT_FOUND(404,"品牌没查到"),
     SPEC_GROUP_NOT_FOUND(404,"商品规格组不存在！"),
     SPEC_PARAM_NOT_FOUND(404,"商品规格参数不存在！"),
+    GOODS_NOT_FOUND(404,"商品不存在！"),
     BRAND_SAVE_ERROR(500,"新增品牌失败"),
     UPLOAD_FILE_ERROR(500,"文件上传失败"),
     INVALID_FILE_TYPE(500,"无效的文件类型"),
